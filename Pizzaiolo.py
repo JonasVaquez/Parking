b5406a5b415431a798f2924aa24056c85a5f12c1
JonasVaquez <74057083+JonasVaquez@users.noreply.github.com>
2020-11-18 15:55:29 +0100

    Update Pizzaiolo.py

--- Pizzaiolo.py
+++ Pizzaiolo.py
@@ -14,18 +14,16 @@ class Pizza:
         self.crème = crème
         self.oeuf = oeuf
 
-
 class Pizzaiolo:
     """Classe dans laquelle le Pizzaiolo intéragit avec son client et
     confectionne,ou non, la pizza"""
-    def commande(pizza):
+    def commande(pizza,commis):
         """Fonction qui vérifie le stock et confectionne la pizza en faisant
            patienter le client, ou l'avertit si il ne peut pas faire la
            pizza par manque d'ingrédients"""
-        if verifStock(pizza) == True:            #vérifie le stock
-            f"Je vous prépare votre pizza !"    #soustrait les ingrédients utilisés au sotck et avertit le client de la confectioin de la pizza
-            retireStock(pizza)      
+        if commis.verifStock(pizza) == True:            #vérifie le stock
+            f"Je vous prépare votre pizza !"    #soustrait les ingrédients utilisés au sotck et avertit le client de la confectioin de la pizza    
         else :
             f"Je suis désolé, mais je n'ai plus les ingrédients nécessaires pour faire votre pizza. Commnderez vous autre chose ?"
-            demandeStock(pizza)                 #demande au commis le stock nécessaire pour faire la pizza demandée par le client et avertit le client que la pizza est indisponible 
+            commis.commande(pizza)                 #commande au commis le stock nécessaire pour faire la pizza demandée par le client et avertit le client que la pizza est indisponible 
     
